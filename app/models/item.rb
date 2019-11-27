cb8d5f8617515d177914e1e92cd287eac2ec9f7b
mariannehbt <marianne.heribert@gmail.com>
2019-11-27 01:41:38 +0100

    [OK] model Item validation - marianne

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,16 @@ class Item < ApplicationRecord
   has_many :orders_items, foreign_key: 'item_id'
   has_many :orders, through: :orders_items, source: :orders_id
 
+  validates :title,
+    presence: true
+
+  validates :description,
+    presence: true
+
+  validates :price,
+    presence: true
+
+  validates :image_url,
+    presence: true
 
 end
