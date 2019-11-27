f7bde70907d7d406712fbc64483e3087a5974362
warsou <marceline.lebourdais@gmail.com>
2019-11-27 15:51:33 +0100

    before action

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
+  before_action :user_signed_in?
   # GET /carts
   # GET /carts.json
   def index
